c9170355b3833e582b70d1c53885b2ef17b12a56
Theo Tyburn <theo.tyburn@plasma.city>
2019-06-17 01:06:53 +0200

    adaptation for windows

--- programms/utils/utils.py
+++ programms/utils/utils.py
@@ -2,6 +2,6 @@ from pathlib import Path
 import os
 from os.path import dirname
 
-def get_project_root() -> str:
+def get_project_root():
     """Returns project root folder."""
     return os.path.abspath(dirname(dirname(dirname(__file__))))
